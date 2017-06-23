fe2d912b83f1f012eaa3f79a35568c5518bd475f
amanzoni <adrien.manzoni@magnolia-cms.com>
2017-06-23 10:39:02 +0200

    Move to front-end-concept (More generic)

---
+++ front-end-concept-integration/src/main/java/info/magnolia/concept/frontend/integration/FrontEndConceptIntegrationModule.java
@@ -0,0 +1,28 @@
+/**
+ * This file Copyright (c) 2016 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Magnolia Network Agreement
+ * which accompanies this distribution, and is available at
+ * http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.concept.frontend.integration;
+
+/**
+ * This class is optional and represents the configuration for the
+ * frontend-concept-integration module. By exposing simple getter/setter/adder
+ * methods, this bean can be configured via content2bean using the properties
+ * and node from <tt>config:/modules/frontend-concept-integration</tt>. If you
+ * don't need this, simply remove the reference to this class in the module
+ * descriptor xml.
+ */
+public class FrontEndConceptIntegrationModule {
+    /* you can optionally implement info.magnolia.module.ModuleLifecycle */
+
+}
